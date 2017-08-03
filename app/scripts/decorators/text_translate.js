e36326291060633aa5cacf14be6d6b3a9d68cd52
jcamilosarmientor <jcamilosarmientor@gmail.com>
2017-08-03 11:18:54 -0500

    Agregada internacionalización
    Se agrego la internacionalización al formulario de aportante

--- app/scripts/decorators/text_translate.js
+++ app/scripts/decorators/text_translate.js
@@ -1,30 +1,58 @@
 "use strict";
 
 /**
- * @ngdoc function
- * @name ssClienteApp.decorator:TextTranslate
- * @description
- * # TextTranslate
- * Decorator of the ssClienteApp
- */
+* @ngdoc function
+* @name ssClienteApp.decorator:TextTranslate
+* @description
+* # TextTranslate
+* Decorator of the ssClienteApp
+*/
 var text_es = {
   TITULO: "GENERATOR-OAS",
   MENSAJE_INICIAL: "Ahora puede comenzar con el desarrollo ...",
   ACTIVO: "Activo",
-  PENSION: "Pensión"
+  PENSION: "Pensión",
+  BTN: {
+    GUARDAR: "Guardar",
+    BUSCAR: "Buscar"
+  },
+  APORTANTE: {
+    INFORMACION_APORTANTE: "Información del Aportate",
+    NOMBRE: "Nombre",
+    IDENTIFICACION: "Identificación",
+    COD_ARL: "Código de ARL",
+    COD_DEPARTAMENTO: "Código Departamento",
+    COD_MUNICIPIO: "Código Municipio"
+  },
+  ACTIVO: {
+    ANIO: "Año",
+    MES: "Mes"
+  }
+
 };
 
 var text_en = {
   TITULO: "GENERATOR-OAS",
   MENSAJE_INICIAL: "Now get to start to develop ...",
-  ACTIVO: "Active"
+  ACTIVO: "Active",
+  BTN: {
+    GUARDAR: "Save"
+  },
+  APORTANTE: {
+    INFORMACION_APORTANTE: "Contributor information",
+    NOMBRE: "Name",
+    IDENTIFICACION: "Identification Number",
+    COD_ARL: "ARL Code",
+    COD_DEPARTAMENTO: "Departament Code",
+    COD_MUNICIPIO: "Municipality Code"
+  }
 };
 
 angular.module('ssClienteApp')
-  .config(function($translateProvider) {
-    $translateProvider
-      .translations("es", text_es)
-      .translations("en", text_en);
-    $translateProvider.preferredLanguage("es");
-    $translateProvider.useSanitizeValueStrategy("sanitizeParameters");
-  });
+.config(function($translateProvider) {
+  $translateProvider
+  .translations("es", text_es)
+  .translations("en", text_en);
+  $translateProvider.preferredLanguage("es");
+  $translateProvider.useSanitizeValueStrategy("sanitizeParameters");
+});
