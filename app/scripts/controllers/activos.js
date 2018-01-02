4a92ffe0b18fd7ec4a630b584a332f4c5af1a7c1
jcamilosarmientor <jcamilosarmientor@gmail.com>
2018-01-02 11:09:59 -0500

    refactor(activos): se disminuyo la cantidad de código para generar los descuentos
    Se eliminaron funciones que ya no se necesitaban o que podían ser disminuidas. También se mejoro el uso de la directiva loadingcircle en este módulo.

--- app/scripts/controllers/activos.js
+++ app/scripts/controllers/activos.js
@@ -8,24 +8,20 @@
 * Controller of the ssClienteApp
 */
 angular.module('ssClienteApp')
-.controller('ActivosCtrl', function (seguridadSocialService, titanCrudService, seguridadSocialCrudService, agoraService, argoService, $scope, $translate, $http) {
+.controller('ActivosCtrl', function (seguridadSocialService, titanCrudService, seguridadSocialCrudService, agoraService, argoService, $scope, $translate) {
   var self = this;
   var dataDescuentos = [];
   var nominaObj;   // Objeto json con la nómina seleccionada
   var contratos = [];
-  var contratistas = []; // Guarda la información del servicio de contrato_general
-  var personasNaturales = []; // Guarda la información del servicio de informacion_persona_natural
 
   self.novedadesDiv = false;
-  self.activosDiv = false;
 
   self.anioPeriodo = new Date().getFullYear();
   self.mesPeriodo = new Date().getMonth();
 
   self.anios = []; // Tiene todos los años desde 1900
-  self.concpSegSoc = []; // Tiene la información de los conceptos correspondientes a pagos
+  var concpSegSoc = []; // Tiene la información de los conceptos correspondientes a pagos
 
-  var fechaActual = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate();
   self.meses = { 1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio",
   7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre" };
 
@@ -40,18 +36,13 @@ angular.module('ssClienteApp')
   titanCrudService.get('concepto_nomina','limit=0&query=NaturalezaConcepto.Nombre:seguridad_social').then(function (response) {
     for (var i = 0; i < response.data.length; i++) {
       if (response.data[i].AliasConcepto.includes("Pago") || response.data[i].AliasConcepto.includes("pago")) {
-        self.concpSegSoc.push(response.data[i]);
+        concpSegSoc.push(response.data[i]);
       }
     }
   });
 
   //Trae las nóminas liquidadas de acuerdo al mes y año seleccionado
   self.buscarNomina = function() {
-    // Devuelve el número de días que tiene el mes
-    function daysInMonth(humanMonth, year) {
-      return new Date(year || new Date().getFullYear(), humanMonth, 0).getDate();
-    }
-    var maxDias = daysInMonth(self.mesPeriodo, self.anioPeriodo).toString();
     titanCrudService.get('preliquidacion', 'query=EstadoPreliquidacion.Activo:true,EstadoPreliquidacion.Nombre:EnOrdenPago,Mes:'+self.mesPeriodo+',Ano:'+self.anioPeriodo)
     .then(function(response) {
       if (response.data !== null) {
@@ -67,32 +58,21 @@ angular.module('ssClienteApp')
     });
   };
 
-  argoService.get('contrato_general','limit=-1').then(function(response) {
-    self.contratistas = response.data;
-  });
-
-  agoraService.get('informacion_persona_natural','limit=-1').then(function(response) {
-    self.personasNaturales = response.data;
-  });
-
-  self.idPreliquidacion = 0;
-
   self.nominaSeleccionada = function() {
-    var pagosNombre = [];
     dataDescuentos = [];
+    contratos = [];
     nominaObj = JSON.parse(self.nomina);  // Conviente el string de self.nomina a un objetso json
     seguridadSocialService.getServicio('pago/CalcularSegSocial',nominaObj.Id).then(function(response) {
       if (response.data !== null) {
         for (var i in response.data) {
-          dataDescuentos.push(response.data[i])
-          contratos.push(response.data[i].NumeroContrato)
+          dataDescuentos.push(response.data[i]);
+          contratos.push(response.data[i].NumeroContrato);
         }
         self.gridOptions.data = dataDescuentos;
         } else {
           self.gridOptions.data = null;
         }
       });
-      self.activosDiv = true;
     };
 
 
@@ -113,8 +93,8 @@ angular.module('ssClienteApp')
           {
             Mes: parseInt(self.mesPeriodo),
             Anio: parseInt(self.anioPeriodo),
-            Liquidacion: self.idPreliquidacion,
-            TipoLiquidacion: '',
+            Liquidacion: nominaObj.Id,
+            TipoLiquidacion: nominaObj.Nomina.TipoNomina.Nombre,
             EstadoSeguridadSocial: { Id: 1 }
           };
 
@@ -125,8 +105,9 @@ angular.module('ssClienteApp')
             Pagos: []
           };
 
+          var tipoPago = 0, valor = 0;
           for (var i in dataDescuentos) {
-            for (var j in self.concpSegSoc) {
+            for (var j in concpSegSoc) {
               var pago =
               {
                 DetalleLiquidacion: dataDescuentos[i].IdDetallePreliquidacion,
@@ -135,72 +116,46 @@ angular.module('ssClienteApp')
                 EntidadPago: 0,
                 PeridodoPago: periodo_pago
               };
-              var tipoPago = 0, valor = 0;
-              switch(self.concpSegSoc[j].NombreConcepto) {
+              switch(concpSegSoc[j].NombreConcepto) {
                 case "arl":
-                var tipoPago = self.concpSegSoc[j].Id;
-                var valor = dataDescuentos[i].Arl;
+                  tipoPago = concpSegSoc[j].Id;
+                  valor = dataDescuentos[i].Arl;
                 break;
                 case "pension_ud":
-                var tipoPago = self.concpSegSoc[j].Id;
-                var valor = dataDescuentos[i].PensionUd;
+                  tipoPago = concpSegSoc[j].Id;
+                  valor = dataDescuentos[i].PensionUd;
                 break;
                 case "salud_ud":
-                var tipoPago = self.concpSegSoc[j].Id;
-                var valor = dataDescuentos[i].SaludUd;
+                  tipoPago = concpSegSoc[j].Id;
+                  valor = dataDescuentos[i].SaludUd;
                 break;
                 case "icbf":
-                var tipoPago = self.concpSegSoc[j].Id;
-                var valor = dataDescuentos[i].Icbf;
+                  tipoPago = concpSegSoc[j].Id;
+                  valor = dataDescuentos[i].Icbf;
                 break;
                 //caja_compensacion
                 default:
-                var tipoPago = self.concpSegSoc[j].Id;
-                var valor = dataDescuentos[i].Caja;
+                  tipoPago = concpSegSoc[j].Id;
+                  valor = dataDescuentos[i].Caja;
                 break;
               }
-              pago["Valor"] = valor;
-              pago["TipoPago"] = tipoPago;
+              pago.Valor = valor;
+              pago.TipoPago = tipoPago;
               transaccion.Pagos.push(pago);
             }
           }
 
-          console.log(transaccion);
-          self.t = transaccion
 
-/*
-          var PeriodoPago = {Mes: 11, Anio: 2018, Liquidacion: 31, TipoLiquidacion: '', EstadoSeguridadSocial: { Id: 1 }}
-          seguridadSocialCrudService.post('periodo_pago', PeriodoPago).then(function (response) {
-            console.log('como entrenar a mi dragon 2');
-          }); */
-
-
-
-/*$http.post('http://localhost:8085/v1/pago', transaccion).then(function(response) {
-  console.log('más diciente por si las moscas.');
-});*/
           seguridadSocialService.post('pago/RegistrarPagos', transaccion).then(function(response) {
-            if (response.data = "Ok") {
+            if (response.data.Code === "Ok") {
               swal(
                 $translate.instant('ALERTAS.REGISTRADO'),
                 $translate.instant('ACTIVOS.REGISTRO')+' <b>'+self.meses[""+self.mesPeriodo+""]+'</b>',
                 'success'
-              )
+              );
+            self.gridOptions.data = null;
             }
           });
-
-          /*
-          seguridadSocialService.post('pago/RegistrarPagos', transaccion).then(function(response) {
-            if (response.data[0] = "Ok") {
-              swal(
-                $translate.instant('ALERTAS.REGISTRADO'),
-                $translate.instant('ACTIVOS.REGISTRO')+' <b>'+self.meses[""+self.mesPeriodo+""]+'</b>',
-                'success'
-              )
-            }
-          });
-        }*/
-
       }
     });
   };
