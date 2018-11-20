7f5da0e1f1dcee67a047b92f84131125eb853134
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-11-20 17:47:34 -0500

    Se corrigio la información a mostrar * Se corrigio la información a mostrar de las personas

--- app/scripts/controllers/ver_incapacidades.js
+++ app/scripts/controllers/ver_incapacidades.js
@@ -8,23 +8,29 @@
 * Controller of the ssClienteApp
 */
 angular.module('ssClienteApp')
-.controller('VerIncapacidadesCtrl', function (titanCrudService) {
+.controller('VerIncapacidadesCtrl', function (titanCrudService, administrativaAmazonService) {
   var self = this;
-    titanCrudService.get('concepto_por_persona','query=Concepto.naturaleza:seguridad_social,Concepto.NombreConcepto__startswith:incapacidad')
+
+    titanCrudService.get('concepto_nomina_por_persona','query=Concepto.NaturalezaConcepto.Nombre:seguridad_social,Concepto.NombreConcepto__startswith:incapacidad')
     .then(function(response) {
       var concepto_por_persona = response.data;
 
       angular.forEach(concepto_por_persona,function(data){
-        titanCrudService.get('informacion_proveedor', 'limit=-1&fields=Id,NomProveedor&query=Id:' + data.Persona.Id).then(function(response) {
-          self.nombre = response.data[0].NomProveedor;
+        administrativaAmazonService.get('contrato_general/'+data.NumeroContrato, '').then(function(response) {
+          administrativaAmazonService.get('informacion_proveedor/'+response.data.Contratista, '').then(function(response) {
+            console.log("response: ", response.data);
+          
           self.gridOptions.data.push({
-            Persona: data.Persona.Id,
-            Nombre: self.nombre,
+            Contrato: data.NumeroContrato,
+            Nombre: response.data.NomProveedor,
             Concepto: data.Concepto,
             FechaDesde: data.FechaDesde,
             FechaHasta: data.FechaHasta});
+          })
+
           });
         });
+        console.log("data: ",self.gridOptions.data);
       });
 
       self.gridOptions = {
@@ -34,7 +40,7 @@ angular.module('ssClienteApp')
         showTreeExpandNoChildren : false,
 
         columnDefs : [
-          {field: 'Persona', visible : false},
+          {field: 'Contrato', headerCellTemplate: '<div align="center">Número de Contrato</div>'},
           {field: 'Nombre', visible: true, width: '30%', headerCellTemplate: '<div align="center">Nombre</div>'},
           {field: 'Concepto.AliasConcepto', visible: true,  headerCellTemplate: '<div align="center">Tipo</div>'},
           {
