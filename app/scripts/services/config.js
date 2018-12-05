549c43b88f7e1dfb912f50d9ebc9ab2b5faa5017
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-12-05 11:19:54 -0500

    Ajustando archivo de configuración a pruebas

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -44,11 +44,12 @@
      // AGORA_SERVICE: "http://10.20.0.254/administrativa_amazon_api/v1/",
     AGORA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/',
     ARGO_SERVICE: "http://10.20.0.254/administrativa_amazon_api/v1/",
-    TITAN_MID_SERVICE: "http://localhost:8082/v1/",
-    TITAN_CRUD_SERVICE: "http://localhost:8080/v1/",
+    TITAN_MID_SERVICE: "http://10.20.0.254/titan_api_mid/v1/",
+    TITAN_CRUD_SERVICE: "http://10.20.0.254/titan_api_crud/v1/",
     RULER: "http://10.20.0.254/ruler/v1/",
     ADMINISTRATIVA_AMAZON_SERVICE: "http://10.20.0.254/administrativa_amazon_api/v1/",
-    SS_CRUD_SERVICE: "http://localhost:8086/v1/",
+    TITAN_MID_SERVICE: "http://10.20.0.254/ss_mid_api/v1/",
+    TITAN_CRUD_SERVICE: "http://10.20.0.254/ss_crud_api/v1/",
     SS_MID_SERVCE: "http://localhost:8085/v1/",
     // SESIONES_SERVICE:"https://autenticacion.portaloas.udistrital.edu.co/apioas/sesiones_crud/v1/",
     CONFIGURACION_SERVICE: "http://10.20.0.254/configuracion_api/v1/",
@@ -78,8 +79,8 @@ var conf_local = {
     // AGORA_SERVICE: "http://10.20.0.254/administrativa_amazon_api/v1/",
     AGORA_SERVICE: 'https://tuleap.udistrital.edu.co/go_api/administrativa_amazon_api/v1/',
     ARGO_SERVICE: "https://tuleap.udistrital.edu.co/go_api/administrativa_amazon_api/v1/",
-    TITAN_MID_SERVICE: "http://localhost:8082/v1/",
-    TITAN_CRUD_SERVICE: "http://localhost:8080/v1/",
+    TITAN_MID_SERVICE: "http://10.20.0.254/titan_api_mid/v1/",
+    TITAN_CRUD_SERVICE: "http://10.20.0.254/titan_api_crud/v1/",
     RULER: "http://10.20.0.254/ruler/v1/",
     ADMINISTRATIVA_AMAZON_SERVICE: "https://tuleap.udistrital.edu.co/go_api/administrativa_amazon_api/v1/",
     SS_CRUD_SERVICE: "http://localhost:8086/v1/",
@@ -120,5 +121,5 @@ var conf_cloud_preproduccion = {
  */
 angular.module("ssClienteApp")
   .constant("CONF", {
-      GENERAL: conf_local
+      GENERAL: conf_pruebas
   });
