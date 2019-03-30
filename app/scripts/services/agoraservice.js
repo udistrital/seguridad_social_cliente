eb795dd972d7ba06e4b580650e323fccac6198ca
Jhon G Castellanos <jgcastellanosj@correo.udistrital.edu.co>
2019-03-29 20:52:48 -0500

    cableado middleware

--- app/scripts/services/agoraservice.js
+++ app/scripts/services/agoraservice.js
@@ -8,7 +8,7 @@
  * Service in the ssClienteApp.
  */
 angular.module('ssClienteApp')
-  .service('agoraService', function ($http, CONF) {
+  .service('agoraService', function ($http, CONF, token_service) {
 
     // var path = 'http://10.20.0.254/administrativa_amazon_api/v1/';
     var path = CONF.GENERAL.AGORA_SERVICE;
@@ -16,19 +16,19 @@ angular.module('ssClienteApp')
     // Public API here
     return {
       getServicio: function(servicio, parametro) {
-        return $http.get(path+servicio+"/"+parametro);
+        return $http.get(path+servicio+"/"+parametro, token_service.getHeader());
       },
       get: function (tabla,params) {
-        return $http.get(path+tabla+"/?"+params);
+        return $http.get(path+tabla+"/?"+params, token_service.getHeader());
       },
       post: function (tabla,elemento) {
-        return $http.post(path+tabla,elemento);
+        return $http.post(path+tabla,elemento, token_service.getHeader());
       },
       put: function (tabla,id,elemento) {
-        return $http.put(path+tabla+"/"+id,elemento);
+        return $http.put(path+tabla+"/"+id,elemento, token_service.getHeader());
       },
       delete: function (tabla,id) {
-        return $http.delete(path+tabla+"/"+id);
+        return $http.delete(path+tabla+"/"+id, token_service.getHeader());
       }
     };
   });
--- app/scripts/services/rulerservice.js
+++ app/scripts/services/rulerservice.js
@@ -8,22 +8,22 @@
  * Service in the ssClienteApp.
  */
 angular.module('ssClienteApp')
-  .service('rulerservice', function ($http, CONF) {
+  .service('rulerservice', function ($http, CONF, token_service) {
 
     var path = CONF.GENERAL.RULER;
 
     return {
       get: function (tabla,params) {
-        return $http.get(path+tabla+"/?"+params);
+        return $http.get(path+tabla+"/?"+params, token_service.getHeader());
       },
       post: function (tabla,elemento) {
-        return $http.post(path+tabla,elemento);
+        return $http.post(path+tabla,elemento, token_service.getHeader());
       },
       put: function (tabla,id,elemento) {
-        return $http.put(path+tabla+"/"+id,elemento);
+        return $http.put(path+tabla+"/"+id,elemento, token_service.getHeader());
       },
       delete: function (tabla,id) {
-        return $http.delete(path+tabla+"/"+id);
+        return $http.delete(path+tabla+"/"+id, token_service.getHeader());
       }
     };
 
