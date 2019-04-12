6a55ea48df28cfe9bf4c4d1f4ad3fa4a409799ba
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2019-04-12 14:19:31 -0500

    Corrigiendo nombres

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
     // Test wich SonarQube
     grunt.loadNpmTasks('grunt-sonar-runner');
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
             livereload: {
                 options: {
                     open: true,
-                    middleware: function (connect) {
+                    middleware: function(connect) {
                         return [
                             connect.static('.tmp'),
                             connect().use(
@@ -100,7 +100,7 @@ module.exports = function (grunt) {
             test: {
                 options: {
                     port: 9101,
-                    middleware: function (connect) {
+                    middleware: function(connect) {
                         return [
                             connect.static('.tmp'),
                             connect.static('test'),
@@ -432,28 +432,28 @@ module.exports = function (grunt) {
             ]
         },
 
-        // sonar
-        sonarRunner: {
-            analysis: {
-                options: {
-                    debug: true,
-                    separator: '\n',
-                    dryRun: false,
-                    sonar: {
-                        host: {
-                            url: 'http://10.20.0.77:9000'
-                        },
-                        projectKey: 'titan_cliente',
-                        projectName: 'titan_cliente',
-                        projectVersion: '0.1',
-                        sources: ['app', 'test'].join(','),
-                        language: 'js',
-                        sourceEncoding: 'UTF-8'
-                    }
-                }
+            // sonar
+    sonarRunner: {
+        analysis: {
+          options: {
+            debug: true,
+            separator: '\n',
+            dryRun: false,
+            sonar: {
+              host: {
+                url: 'http://10.20.0.77:9000'
+              },
+              projectKey: 'ss_cliente',
+              projectName: 'ss_cliente',
+              projectVersion: '0.1',
+              sources: ['app','test'].join(','),
+              language: 'js',
+              sourceEncoding: 'UTF-8'
             }
-        },
-        // fin sonar
+          }
+        }
+      },
+      // fin sonar
 
         // Test settings
         karma: {
@@ -465,7 +465,7 @@ module.exports = function (grunt) {
     });
 
 
-    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+    grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
         if (target === 'dist') {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
         }
@@ -480,7 +480,7 @@ module.exports = function (grunt) {
         ]);
     });
 
-    grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
+    grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function(target) {
         grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
         grunt.task.run(['serve:' + target]);
     });
